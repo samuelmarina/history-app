381c7ebba7b9f66d695c07e2534cbe3a904b8bc0
samuellmarina <samuel.marina@fidecorp.com>
2022-04-17 22:52:06 -0400

    Initial Commit

---
+++ History App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  History App
+//
+//  Created by Samuel Miller on 4/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
